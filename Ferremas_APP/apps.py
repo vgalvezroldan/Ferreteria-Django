651ed9d0b8cf73b451a10e6f12dfea91039fa643
vgalvezroldan <vgalvezroldan@gmail.com>
2024-05-06 23:00:39 -0400

    Modelos ORM

---
+++ Ferremas_APP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FerremasAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Ferremas_APP'
---
+++ Ferremas_APP/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField()
+
+class Carrito(models.Model):
+    sesion_id = models.CharField(max_length=100)
+
+class CarritoProducto(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+
